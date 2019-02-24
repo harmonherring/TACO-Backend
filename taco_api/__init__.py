bdd9b50df894b093325faeb51172d4c9551af60a
chj8702 <chj8702@rit.edu>
2019-02-24 00:11:37 -0500

    basic routes

--- taco_api/__init__.py
+++ taco_api/__init__.py
@@ -17,3 +17,14 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 @app.route('/', methods=['GET'])
 def test():
     return "fuck you"
+
+@app.route('/client/register', methods=['GET'])
+def verify():
+    #returns a unique UID
+    return False
+
+@app.route('/client/<uid>', methods=['GET'])
+def return_tasks():
+    #indexes Tasks DB and returns tasks for the specifed uid
+    return False
+
