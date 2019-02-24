faf43a788abca484b1ffbd0cc2542aae006f0080
Harmon Herring <harmonherring@gmail.com>
2019-02-23 23:29:50 -0600

    Change default IP

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from taco_api import app
 import os
 if __name__ == '__main__':
-    app.run(port=os.environ.get('API_PORT', 8080))
+    app.run(ip=os.environ.get('API_IP', 0.0.0.0), port=os.environ.get('API_PORT', 8080))
 
 application = app
