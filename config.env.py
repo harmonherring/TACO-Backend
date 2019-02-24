4656042109a91327c6012194e9973bd9fbfd909d
Harmon Herring <harmonherring@gmail.com>
2019-02-24 18:50:18 -0500

    Interfaces with tables in the database, can retrieve all clients and all tasks at specified endpoint

--- config.env.py
+++ config.env.py
@@ -5,4 +5,4 @@ IP = os.environ.get('API_IP', "0.0.0.0")
 PORT = os.environ.get('API_PORT', 8080)
 
 # Database
-SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', "")
