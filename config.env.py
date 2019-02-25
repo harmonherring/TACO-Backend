e6e14adf03edc9a09ab5c3ea71aa949ce87d8b48
Harmon Herring <harmonherring@gmail.com>
2019-02-25 09:26:21 -0500

    CORS

---
+++ config.env.py
@@ -0,0 +1,9 @@
+import os
+
+# Flask config
+IP = os.environ.get('API_IP', "0.0.0.0")
+PORT = os.environ.get('API_PORT', 8080)
+
+# Database
+SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+
